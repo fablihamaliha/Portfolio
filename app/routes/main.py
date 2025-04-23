7868c6b45ec95d92a0cf86e895aa1149efebb352
Maliha <fablihamaliha123@gmail.com>
2025-04-22 23:40:11 -0700

    Adding Updates for now

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,14 +1,16 @@
+import requests
 from flask import Blueprint, render_template
+
 
 main_bp = Blueprint('main', __name__)
 
 @main_bp.route('/')
-def home():
-    print("🔁 / route called")
+def base():
 
-    return render_template('home.html')
-
-
+    response = requests.get(
+        "http://api.weatherapi.com/v1/current.json?key=3ca3bfa286b842afb4242203252304&q=San Francisco&aqi=no")
+    data = response.json
+    return render_template('base.html')
 @main_bp.route('/journey')
 def journey():
     print("🔁 / journey route called")
@@ -26,3 +28,11 @@ def certifications():
     print("🔁 / cert route called")
 
     return render_template('certifications.html')
+
+
+
+@main_bp.route('/contact')
+def contact():
+    print("🔁 / contac route called")
+
+    return render_template('contact.html')
