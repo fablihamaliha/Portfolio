26f19ce325c5a0a656a0968e9fa88e5ac58fe84e
Maliha <fablihamaliha123@gmail.com>
2025-04-24 00:07:18 -0700

    Adding Updates for now

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ from app.routes.main import main_bp  # adjust if needed
 from flask import Flask
 
 def create_app():
-    app = Flask(__name__, template_folder='app/templates')
+    app = Flask(__name__)
     app.static_folder = 'app/static'
     app.register_blueprint(main_bp)
     return app
